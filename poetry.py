520eed0db43cf482197b6328d6374ecaf44cf53d
Cptn Reef <sharif.staffordbz@gmail.com>
2020-10-02 00:13:23 -0400

    Incomplete

--- poetry.py
+++ poetry.py
@@ -1,22 +1,37 @@
+import random
+
 def get_file_lines(filename):
     franka = open(filename)
     rdy = franka.read()
     print(rdy)
-
+    franka.close
     return filename
 
-get_file_lines("poem.txt")
-
-print('\n')
-
-def lines_printed_backwards(filename):
-    frankb = open(filename)
+def lines_printed_backwards(lines_list):
+    frankb = open(lines_list)
     lines = frankb.readlines()
     for line in reversed(lines):
         print(line)
 
     frankb.close()
-    return filename
+    return lines_list
 
-lines_printed_backwards("poem.txt")
+def lines_printed_random(lines_list):
+    beef = open(lines_list)
+    lines = beef.readlines()
+    linelen = 21
+    for line in range(linelen):
+        line
+        print(lines[random.randint(1,len(lines)-1)])
+
+    beef.close()
+    return lines_list
+
+def lines_printed_custom(lines_list):
+    custom = open(lines_list)
+    line = custom.readlines()
+    for i in range(len(line)):
+        print(line[-i])
+    custom.close
+    return lines_list
 
